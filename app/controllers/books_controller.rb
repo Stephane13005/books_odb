81bec9939391f4ffce34ca5cdc6228c7decaf81f
Stéphane Severio <severio.stephane@gmail.com>
2022-08-30 18:42:41 +0200

    End of section 3

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,22 +1,49 @@
 class BooksController < ApplicationController
+  before_action :find_book, only: [:show, :edit, :update, :destroy]
+
   def index
     @books = Book.all
   end
 
   def show
-    @book = Book.find(params[:id])
   end
 
   def new
     @book = Book.new
   end
 
+  def edit
+  end
+
   def create
-    @book = Book.new(params.require(:book).permit(:title, :description, :author, :pages_count, :published_at))
+    @book = Book.new(book_params)
     if @book.save
       redirect_to books_path
     else
       render :new
     end
+  end
+
+  def update
+    if @book.update(book_params)
+      redirect_to books_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @book.destroy
+    redirect_to books_path
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :description, :author, :pages_count, :published_at)
+  end
+
+  def find_book
+    @book = Book.find(params[:id])
   end
 end
