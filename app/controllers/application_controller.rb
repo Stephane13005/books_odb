82217142e88b0f92ad07c366098f203c496f1741
Stéphane Severio <severio.stephane@gmail.com>
2022-09-01 17:47:02 +0200

    Internationalisation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+  before_action :set_locale
+
+  def default_url_options
+    { locale: I18n.locale }
+  end
+
+  private
+
+  def set_locale
+     locale = params[:locale] || cookies[:locale] || I18n.default_locale
+     I18n.locale = locale
+     cookies[:locale] = { value: locale, expires: 30.days }
+  end
 end
